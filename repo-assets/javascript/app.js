ef473e4ed068f7036ae8eef3fbc42ed5de439566
Edward Hill <edwardpassanohill@gmail.com>
2019-12-19 14:56:13 -0500

    fix CSS layout, add js  psudocode

---
+++ repo-assets/javascript/app.js
@@ -0,0 +1,47 @@
+// Steps to complete:
+
+// 1. Initialize Firebase
+// 2. Create button for adding new employees - then update the html + update the database
+// 3. Create a way to retrieve employees from the employee database.
+// 4. Create a way to calculate the months worked. Using difference between start and current time.
+//    Then use moment.js formatting to set difference in months.
+// 5. Calculate Total billed
+
+// 1. Initialize Firebase
+
+// 2. Button for adding Employees
+
+// Grabs user input
+
+// Creates local "temporary" object for holding employee data
+
+// Uploads employee data to the database
+
+// Logs everything to console
+
+// Clears all of the text-boxes
+
+// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+
+// Store everything into a variable.
+
+// Employee Info
+
+// Prettify the employee start
+
+// Calculate the months worked using hardcore math
+// To calculate the months worked
+
+// Calculate the total billed rate
+
+// Create the new row
+
+// Append the new row to the table
+
+// Example Time Math
+// -----------------------------------------------------------------------------
+// Assume Employee start date of January 1, 2015
+// Assume current date is March 1, 2016
+
+// We know that this is 15 months.
+// Now we will create code in moment.js to confirm that any attempt we use meets this test case
